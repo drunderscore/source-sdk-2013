0d491f77f367c6bb30f4ae1ac231d2f7ec48d632
Matt Haynie <mrhaynie@live.com>
2018-08-01 13:33:51 -0700

    New shaders for new outlines.

--- mp/src/materialsystem/stdshaders/buildceshaders.bat
+++ mp/src/materialsystem/stdshaders/buildceshaders.bat
@@ -1,6 +1,6 @@
 @echo off
 
-taskkill /IM shadercompile.exe
+taskkill /F /IM shadercompile.exe
 
 call buildshaders.bat CEShaders_dx9_20b -game "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf" -source "C:\Users\matt\source\repos\CastingEssentials\sdk2013\mp\src"
 
