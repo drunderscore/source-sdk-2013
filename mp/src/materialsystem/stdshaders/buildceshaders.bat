ff78034fbfeb5b55529bb6f1a0972b728849dcdc
Matt Haynie <mrhaynie@live.com>
2018-08-16 14:10:49 -0700

    Added NVIDIA FXAA 3.11.

--- mp/src/materialsystem/stdshaders/buildceshaders.bat
+++ mp/src/materialsystem/stdshaders/buildceshaders.bat
@@ -3,5 +3,6 @@
 taskkill /F /IM shadercompile.exe
 
 call buildshaders.bat CEShaders_dx9_20b -game "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf" -source "C:\Users\matt\source\repos\CastingEssentials\sdk2013\mp\src"
+call buildshaders.bat CEShaders_dx9_30 -game "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf" -source "C:\Users\matt\source\repos\CastingEssentials\sdk2013\mp\src"
 
 copy "shaders\fxc\*" "..\..\..\..\..\plugin_folder\shaders\fxc\"
