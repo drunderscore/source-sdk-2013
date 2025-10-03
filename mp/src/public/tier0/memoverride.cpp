6302d06b32c9ef01099e5326d1b5296f79c1d69a
resin <hey@amberres.in>
2025-10-03 17:19:17 -0400

    Correct signature for CRT function redefinition of `_msize_base`
    Newer versions of the Windows Kit headers have an exception
    specification on this function, and missing it results in an error.
    
    Previously the build was requiring the use of an older SDK, but since
    GitHub machines have removed these files, my hand was forced yet again.

--- mp/src/public/tier0/memoverride.cpp
+++ mp/src/public/tier0/memoverride.cpp
@@ -274,7 +274,7 @@ ALLOC_CALL void * __cdecl _recalloc ( void * memblock, size_t count, size_t size
 	return pMemOut;
 }
 
-size_t _msize_base( void *pMem )
+size_t _msize_base( void *pMem ) _CRT_NOEXCEPT
 {
 	return g_pMemAlloc->GetSize(pMem);
 }
